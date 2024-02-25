4cf8e03db8b9ad46c21ba057ace14d301f9d9600
Grem <lemonman738@gmail.com>
2024-02-25 11:33:04 +0530

    Update stats.py

--- api/stats.py
+++ api/stats.py
@@ -21,20 +21,37 @@ USERNAME = "Grem6"
 @app.route("/api/followers")
 def followers_count():
     url = f"https://api.github.com/users/{USERNAME}"
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     data = response.json()
     followers = data.get("followers", 0)
-    return jsonify({"followers": followers})
+    return str(followers)
 
 
 # Endpoint to fetch repository count
 @app.route("/api/repositories")
 def repositories_count():
     url = f"https://api.github.com/users/{USERNAME}/repos"
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     data = response.json()
     repositories = len(data)
-    return jsonify({"repositories": repositories})
+    return str(repositories)
+
+
+# Endpoint to fetch number of commits
+@app.route("/api/commits")
+def commits_count():
+    url = f"https://api.github.com/users/{USERNAME}/events"
+    response = requests.get(url)
+    commits_count = len(
+        [event for event in response.json() if event["type"] == "PushEvent"]
+    )
+    return str(commits_count)
+
+
+# Endpoint for the root path
+@app.route("/")
+def index():
+    return "Welcome to the GitHub API Wrapper"
 
 
 if __name__ == "__main__":
